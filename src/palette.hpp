9d787acc1cf649f81670b432b6d99fd72a076e3c
Roy Crihfield <roy@manteia.ltd>
2020-08-09 16:51:39 -0500

    Revert "cleanup"
    This reverts commit 7363de6b800f0b3a4a9774b9d2093e6bdb9dbb08.

---
+++ src/palette.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Rxt/color.hpp>
+
+#include <map>
+#include <string>
+
+using color_palette = std::map<std::string, Rxt::rgba>;
+
+inline color_palette default_palette()
+{
+    using namespace Rxt;
+    color_palette p;
+    p.emplace("hl", to_rgba(colors::white));
+    p.emplace("bg", to_rgba(colors::black));
+    p.emplace("soil", to_rgba(colors::soil));
+    p.emplace("sand", to_rgba(colors::sand));
+    p.emplace("water", to_rgba(colors::blue));
+    p.emplace("hl_water", to_rgba(colors::blue));
+    return p;
+}
