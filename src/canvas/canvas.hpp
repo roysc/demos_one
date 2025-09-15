4582086cfb26829f2f7d2aa0896d96fd3c83f68c
Roy Crihfield <roy@manteia.ltd>
2025-09-15 22:05:18 +0800

    rxt reactive clean up

--- src/canvas/canvas.hpp
+++ src/canvas/canvas.hpp
@@ -26,8 +26,8 @@ using grid_selector = mouse_select_tool<int>;
 using grid_painter = mouse_paint_tool<int>;
 using stroke_tool = mouse_stroke_tool<int>;
 
-using viewport_type = Rxt::wrap_reactive_crt<Rxt::reactive_viewport, hooks<>, int>;
-using cursor_type = Rxt::wrap_reactive_crt<Rxt::reactive_cursor, hooks<>, int>;
+using viewport_type = Rxt::reactive_viewport<int>;
+using cursor_type = Rxt::reactive_cursor<int>;
 
 // Set of hooks to be swapped out whenever the tool changes
 struct tool_hooks
